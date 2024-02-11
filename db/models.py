ab0d62eae467f6617e1e05ca0738fae145d4c516
My Name <Me@example.com>
2024-02-11 12:37:38 +0100

    jaja mein führer

---
+++ db/models.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String
+from .engine import Base
+
+class DBUser(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, unique=True, index=True)
+    username = Column(String, unique=True, nullable=False)
+    email = Column(String, unique=True, nullable=False)
